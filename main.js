11135df22f43ece254f512dca62fa2d7b05bed71
Tarun <45634649+tarun18k@users.noreply.github.com>
2020-04-11 01:42:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function closeForm3() {
 }
 
 var form = document.getElementById('myform');
-var maindiv = document.getElementById('pending');
+var maindiv = document.getElementById('requirements');
 var button = document.getElementById('ckt');
 var inputtitle = document.getElementById('title');
 var inputdesc = document.getElementById('desc');
@@ -39,6 +39,36 @@ var inputprior = document.getElementById('priority');
 var inputstatus = document.getElementById('status');
 var inputcat = document.getElementById('createdat');
 var style=document.createElement('style');
+
+var form1 = document.getElementById('myform1');
+var maindiv1 = document.getElementById('design');
+var button1 = document.getElementById('ckt1');
+var inputtitle1 = document.getElementById('title1');
+var inputdesc1 = document.getElementById('desc1');
+var inputprior1 = document.getElementById('priority1');
+var inputstatus1 = document.getElementById('status1');
+var inputcat1 = document.getElementById('createdat1');
+
+
+var form2 = document.getElementById('myform2');
+var maindiv2 = document.getElementById('development');
+var button2 = document.getElementById('ckt2');
+var inputtitle2 = document.getElementById('title2');
+var inputdesc2 = document.getElementById('desc2');
+var inputprior2 = document.getElementById('priority2');
+var inputstatus2 = document.getElementById('status2');
+var inputcat2 = document.getElementById('createdat2');
+
+
+var form3 = document.getElementById('myform3');
+var maindiv3 = document.getElementById('testing');
+var button3 = document.getElementById('ckt3');
+var inputtitle3 = document.getElementById('title3');
+var inputdesc3 = document.getElementById('desc3');
+var inputprior3 = document.getElementById('priority3');
+var inputstatus3 = document.getElementById('status3');
+var inputcat3 = document.getElementById('createdat3');
+
 
 
 
@@ -58,11 +88,73 @@ const divmaker = (text1, text2, text3) => {
     div.appendChild(newContent);
     newContent=document.createElement('br');
     div.appendChild(newContent);
-    newContent = document.createTextNode("Createdat:" + new Date())
+    newContent = document.createTextNode("Created at:" + new Date())
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);  
+}
+const divmaker1 = (text1, text2, text3) => {
+    var div = document.createElement('div');
+    div.className ="sticky";
+    maindiv1.append(div);
+    var newContent = document.createTextNode("title: " + text1 );
     div.appendChild(newContent);
     newContent=document.createElement('br');
     div.appendChild(newContent);
-    
+    newContent = document.createTextNode ("Description:" + text2);
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);
+    newContent = document.createTextNode("Status: " + text3);
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);
+    newContent = document.createTextNode("Created at:" + new Date())
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);  
+}
+const divmaker2 = (text1, text2, text3) => {
+    var div = document.createElement('div');
+    div.className ="sticky";
+    maindiv2.append(div);
+    var newContent = document.createTextNode("title: " + text1 );
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);
+    newContent = document.createTextNode ("Description:" + text2);
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);
+    newContent = document.createTextNode("Status: " + text3);
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);
+    newContent = document.createTextNode("Created at:" + new Date())
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);  
+}
+const divmaker3 = (text1, text2, text3) => {
+    var div = document.createElement('div');
+    div.className ="sticky";
+    maindiv3.append(div);
+    var newContent = document.createTextNode("title: " + text1 );
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);
+    newContent = document.createTextNode ("Description:" + text2);
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);
+    newContent = document.createTextNode("Status: " + text3);
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);
+    newContent = document.createTextNode("Created at:" + new Date())
+    div.appendChild(newContent);
+    newContent=document.createElement('br');
+    div.appendChild(newContent);  
 }
 form.addEventListener('submit',function (e) {
     e.preventDefault()
@@ -73,6 +165,39 @@ form.addEventListener('submit',function (e) {
     inputstatus.value = '';
     inputcat.value = '';
     closeForm();
+
+});
+form1.addEventListener('submit',function (e) {
+    e.preventDefault()
+
+    divmaker1(inputtitle1.value,inputdesc1.value,inputstatus1.value);
+    inputtitle.value = '';
+    inputdesc.value = '';
+    inputstatus.value = '';
+    inputcat.value = '';
+    closeForm1();
+
+});
+form2.addEventListener('submit',function (e) {
+    e.preventDefault()
+
+    divmaker2(inputtitle2.value,inputdesc2.value,inputstatus2.value);
+    inputtitle.value = '';
+    inputdesc.value = '';
+    inputstatus.value = '';
+    inputcat.value = '';
+    closeForm2();
+
+});
+form3.addEventListener('submit',function (e) {
+    e.preventDefault()
+
+    divmaker3(inputtitle3.value,inputdesc3.value,inputstatus3.value);
+    inputtitle.value = '';
+    inputdesc.value = '';
+    inputstatus.value = '';
+    inputcat.value = '';
+    closeForm3();
 
 });
 
