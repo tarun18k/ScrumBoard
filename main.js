abf6e10824744fc2c545383f5a64cd3f8011a5a1
Tarun <45634649+tarun18k@users.noreply.github.com>
2020-04-26 19:41:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,60 @@
+//Divisions Selection
+
+var reqe = document.querySelector('#Requirements');
+var teste = document.querySelector('#testing');
+var deve = document.querySelector('#development');
+var dese = document.querySelector('#design');
+// 
+
+
+//top Container 
 var heading = document.querySelector('h1');
 var form = document.querySelector('.profileform');
 var x = document.querySelector('.profbtn');
-let name = localStorage.getItem('proj') ? JSON.parse(localStorage.getItem('proj')) : [];
-localStorage.setItem('proj', JSON.stringify(name));
-const n = JSON.parse(localStorage.getItem('proj'));
+var n;
+
+//Project Name Div Events
+
 
 x.addEventListener('click', function (E) {
     E.preventDefault();
     var projectname = document.querySelector('input').value;
-    name.push(projectname);
-
-    localStorage.setItem('proj', JSON.stringify(name));
-
-    forminvisible(projectname);
+    localStorage.setItem("ProjectName", projectname);
+    forminvisible(localStorage.getItem("ProjectName"));
 });
 
-
 function forminvisible(value) {
+    if(value!=null)
+    {
     form.classList.add("profileforma");
     heading.innerHTML = "Project :  " + value;
 }
-n.forEach(item => {
-    forminvisible(item);
-});
+}
+forminvisible(localStorage.getItem("ProjectName"));
+// project nameevents
+
+//EditFormButton & Events
+
+
+var editform=document.querySelector('#editform');
+function editformv(){
+    editform.style.display="block";
+}
+
+
+
+function editfromin()
+{
+    editform.style.disply="none;"
+}
 
 
 
 
 
+
+
+// Create Task Form & Events
 var addtsk = document.querySelector('.addtask');
 var mainform = document.querySelector('#mainform');
 
@@ -36,21 +63,30 @@ function forminvisiblemain() {
 
 }
 
+
 function visibleform() {
     mainform.style.display = "block";
 }
 addtsk.addEventListener('click', function () {
     visibleform();
 });
+  
+
+
+
+
+//Create Task Button Functions
 
 
 var ckt = document.querySelector('#ckt');
 
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+var itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+var data = JSON.parse(localStorage.getItem('items'));
 
-function creatediv(input, desc, status, createdat, type) {
+var del;
+var e;
+function creatediv(input, desc, status, type) {
     var div = document.createElement('div');
     div.className = "sticky";
     switch (type) {
@@ -68,7 +104,34 @@ function creatediv(input, desc, status, createdat, type) {
             break;
 
     }
-    div.innerHTML="<p>Title: <strong>"+input+"</strong></p> <p>Description: <strong>"+desc+"</strong></p><p>Status: <strong>"+status+"</strong></p><p>Created At: <strong> "+createdat+"</strong></p><p>Time Created: <strong> "+new Date()+"</strong></p>";}
+    div.innerHTML="<p>Title: <strong>"+input+"</strong></p> <p>Description: <strong>"+desc+"</strong></p><p>Status: <strong>"+status+"</strong></p><p>Created At: <strong> "+new Date()+"</strong></p><button class='deletenote'><i class='fas fa-trash'></i><span class='caption'>DeleteNote</span></button><button class='edit'><i class='fas fa-pencil-alt'></i><span class='caption'>Edit</span></button>";
+    del=document.querySelectorAll('.deletenote');
+    for(var i=0;i<del.length;i++)
+        {
+            del[i].addEventListener('click', function (e) {
+                 this.parentNode.parentNode.removeChild(this.parentNode);
+                });
+  
+        }
+    
+    
+    
+    
+    e=document.querySelectorAll('.edit');
+    for(var i=0;i<e.length;i++)
+        {
+            e[i].addEventListener('click',function()
+                                  {
+                editformv();
+            });
+        }
+}
+
+
+
+
+
+//Create Task ButtonEvent listner
 ckt.addEventListener('click', function (e) {
     e.preventDefault();
     var input = document.querySelector('#title').value;
@@ -81,32 +144,29 @@ ckt.addEventListener('click', function (e) {
     } else {
         var status = document.getElementById('s2').value;
     }
-
-    var createdat = document.querySelector('#createdat').value;
     var priority = document.querySelector('#priority').value;
     var type;
     if (document.getElementById('r1').checked == true) {
-        var type = document.getElementById('r1').value;
+        type = document.getElementById('r1').value;
     } else if (document.getElementById('r2').checked == true) {
-        var type = document.getElementById('r2').value;
+         type = document.getElementById('r2').value;
     } else if (document.getElementById('r3').checked == true) {
-        var type = document.getElementById('r3').value;
+     type = document.getElementById('r3').value;
     } else {
-        var type = document.getElementById('r4').value;
+     type = document.getElementById('r4').value;
     }
-    var div1 = new Adddiv(input, desc, status, createdat, priority, type);
+    var div1 = new Adddiv(input, desc, status,priority, type);
     itemsArray.push(div1);
 
     localStorage.setItem('items', JSON.stringify(itemsArray));
 
-    creatediv(input, desc, status, createdat, type);
+    creatediv(input, desc, status, type);
 
-    forminvisiblemain();
+   forminvisiblemain();
 
     input.value = " ";
     desc.value = " ";
     status.value = " ";
-    createdat.value = " ";
     priority.value = " ";
     type.value = " ";
      window.location.reload();
@@ -114,29 +174,24 @@ ckt.addEventListener('click', function (e) {
 
 });
 
-function Adddiv(input, desc, status, createdat, priority, type) {
+function Adddiv(input, desc, status,  priority, type) {
     this.title = input;
     this.desc = desc;
     this.status = status;
-    this.createdat = createdat;
     this.priority = priority;
     this.type = type;
 }
 
-var reqe = document.querySelector('#Requirements');
-var teste = document.querySelector('#testing');
-var deve = document.querySelector('#development');
-var dese = document.querySelector('#design');
+data.forEach(lse);
+function lse(item){
+     creatediv(item.title, item.desc, item.status,item.type);   
+}
 
 
-
-data.forEach(item => {
-    creatediv(item.title, item.desc, item.status, item.createdat, item.type);
-});
-
-
-var del = document.querySelector('.delete');
-del.addEventListener('click', function () {
-    localStorage.clear();
+var dela=document.querySelector('.delete');
+dela.addEventListener('click',function()
+                     {
+      localStorage.clear();
     window.location.reload();
+
 });
